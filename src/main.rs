5bb2479bec41729af0ade8537e6876ae926714b0
mtaborda <mtaborda401@hotmail.com>
2024-02-29 11:41:19 -0300

    Fix clipboard monitoring and add content to array

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::time::Duration;
 
 fn main() {
     // Inicia un hilo para monitorear el portapapeles
-    let clipboard_thread = std::thread::spawn(|| {
+    let mut store: Vec<String> = Vec::new();
+    let clipboard_thread = std::thread::spawn(move || {
         // Guarda el contenido actual del portapapeles
         let mut clipboard = ClipboardContext::new().expect("No se pudo acceder al portapapeles");
         let mut last_clipboard_content = clipboard.get_contents().unwrap_or_default();
@@ -12,14 +13,15 @@ fn main() {
         loop {
             // Obtiene el contenido actual del portapapeles
             let current_clipboard_content = clipboard.get_contents().unwrap_or_default();
-
+            // add to array
+            store.push(current_clipboard_content.clone());
             // Comprueba si el contenido ha cambiado
             if current_clipboard_content != last_clipboard_content {
-                println!("Contenido del portapapeles cambiado: {}", current_clipboard_content);
+                println!("Contenido del portapapeles cambiado: {:?}", store);
                 // Puedes realizar cualquier acción que desees con el nuevo contenido
 
                 // Actualiza el último contenido conocido del portapapeles
-                last_clipboard_content = current_clipboard_content.clone();
+                last_clipboard_content = current_clipboard_content;
             }
 
             // Espera un breve período antes de verificar nuevamente
